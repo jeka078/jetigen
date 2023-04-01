78fc245154440eb8b296ac0145b3edef5cb6f100
jeka078 <jetigen280718@gmail.com>
2023-04-01 21:56:41 +0600

    jetigen

--- class1.py
+++ class1.py
@@ -1,3 +1,7 @@
 class Hello:
     def __init__(self, str):
         self.str = str
+
+class Jetigen(Hello):
+    def __init__(self, str):
+        super().__init__(str)
--- task.py
+++ task.py
@@ -1,2 +1,5 @@
 from art import tprint
 tprint("PYTHON")
+from class1 import Jetigen
+jetigen = Jetigen('jetigen')
+tprint(jetigen.str)
